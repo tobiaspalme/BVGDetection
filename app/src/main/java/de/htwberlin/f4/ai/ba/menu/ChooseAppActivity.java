c63cf53549eab1461137ed81f840d13227c621d8
HanfBauer <diablo123>
2017-07-17 13:39:36 +0200

    simple menu for choosing which app to start

---
+++ app/src/main/java/de/htwberlin/f4/ai/ba/menu/ChooseAppActivity.java
@@ -0,0 +1,58 @@
+package de.htwberlin.f4.ai.ba.menu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.carol.bvg.MainActivity;
+import com.example.carol.bvg.R;
+
+public class ChooseAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_app);
+
+
+
+        Button btnCarola = (Button) findViewById(R.id.btn_carola);
+        btnCarola.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Carola", Toast.LENGTH_SHORT);
+                toast.show();
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        Button btnJohann = (Button) findViewById(R.id.btn_johann);
+        btnJohann.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Johann", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        Button btnBenjamin = (Button) findViewById(R.id.btn_benjamin);
+        btnBenjamin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Benjamin", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+
+    }
+
+
+
+}
