bf1265df7a1c546d9c76711455c1867f506ccd34
Johann Winter <E-mail>
2017-08-08 15:45:18 +0200

    Removed "Carola"-Part of the App
    + Removed package "com.example/carol/bvg"
    + Removed linkings in Manifest and "ChooseAppActivity"

--- app/src/main/java/de/htwberlin/f4/ai/ba/menu/ChooseAppActivity.java
+++ app/src/main/java/de/htwberlin/f4/ai/ba/menu/ChooseAppActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.carol.bvg.MainActivity;
 import com.example.carol.bvg.R;
 
 import de.htwberlin.f4.ai.ba.coordinates.android.CoordinatesActivity;
@@ -18,20 +17,6 @@ public class ChooseAppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_app);
-
-
-
-        Button btnCarola = (Button) findViewById(R.id.btn_carola);
-        btnCarola.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(), "Carola", Toast.LENGTH_SHORT);
-                toast.show();
-
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-
-            }
-        });
 
         Button btnJohann = (Button) findViewById(R.id.btn_johann);
         btnJohann.setOnClickListener(new View.OnClickListener() {
